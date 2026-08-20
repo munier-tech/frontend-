bce4fc289ff6ee4ab8709ab75972feb5b92bc813
Munir Yusuf <muniry399@gmail.com>
2026-08-20 17:02:32 +0300

    done

--- src/components/attendances/CreateAttendance.jsx
+++ src/components/attendances/CreateAttendance.jsx
@@ -9,7 +9,7 @@ import { toast } from 'react-hot-toast';
 
 // Somali translations
 const translations = {
-  heading: "DiiwaaSngelinta Imaatinka",
+  heading: "DiiwaaSelinta Imaatinka",
   selectClass: "Dooro Fasalka",
   selectDate: "Dooro Taariikhda",
   studentName: "Magaca Ardayga",
