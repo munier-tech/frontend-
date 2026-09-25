4a62714b6da367920ba1122fb36ec5d86226e1ba
Munir Yusuf <muniry399@gmail.com>
2026-09-25 18:37:02 +0300

    done

--- src/components/attendances/CreateAttendance.jsx
+++ src/components/attendances/CreateAttendance.jsx
@@ -11,7 +11,7 @@ import { toast } from 'react-hot-toast';
 const translations = {
   heading: "Diiwelin Imaatinka",
   selectClass: "Dooro Fasalka",
-  selectDate: "Dooro Taariida",
+  selectDate: "Door Taariida",
   studentName: "Maga Ara",
   age: "Da'da",
   gender: "Jinsiga",
